23988f9fc33bc6d10ab782d776b11f434cf659b9
Larissa Araújo <lariaraujo201@gmail.com>
2020-05-21 09:12:31 -0300

    chore: update routes

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 
-import { ReactComponent as LogoSvg } from '../../assets/img/instagram-logo.svg';
+// import { ReactComponent as LogoSvg } from '../../assets/img/instagram-logo.svg';
 
 import './Topbar.scss';
 
 const Topbar = () => (
   <header className="topbar">
     <div className="container">
+      <Link to='/'>Feed</Link>
+      <Link to='/users'>Users</Link>
+      <Link to='/users/1'>Profile</Link>
+      <Link to='/newuser'>New User</Link>
     </div>
   </header>
 );
--- src/routes/NewUserRoute/NewUserRoute.jsx
+++ src/routes/NewUserRoute/NewUserRoute.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
-import UserForm from '../../containers/UserForm';
+// import UserForm from '../../containers/UserForm';
 
-const NewUserRoute = () => (null);
+const NewUserRoute = () => (
+  <h1>New User</h1>
+);
 
 export default NewUserRoute;
