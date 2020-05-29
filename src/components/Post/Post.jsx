9f9465beef7e4dff103d4030f48cb227afd0eafa
Larissa Araújo <lariaraujo201@gmail.com>
2020-05-28 21:27:02 -0300

    chore: some minor updates

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
-// , { useState }
+import React, { useState } from 'react';
 
 import { Link } from 'react-router-dom';
 
 import './Post.scss';
 
 const Post = ({ postInfo, userInfo }) => {
+  const [following, setFollowing] = useState(false);
+  const [like, setLike] = useState(false);
+
   const { imageUrl, comments } = postInfo;
 
   return (
@@ -20,9 +22,14 @@ const Post = ({ postInfo, userInfo }) => {
               <Link to={`/users/${userInfo.username}`} className="user__name">{userInfo.username}</Link>
             </div>
             
-            <button className="post__context">
-              <span className="follow-btn is-following">Seguindo</span>
-              {/* <span className="follow-btn">Seguir</span> */}
+            <button 
+              className="post__context"
+              onClick={() => setFollowing(!following)}
+            >
+              { following 
+                ? <span className="follow-btn is-following">Seguindo</span>
+                : <span className="follow-btn">Seguir</span> 
+              }
             </button>
           </div>
         }
@@ -33,14 +40,24 @@ const Post = ({ postInfo, userInfo }) => {
 
         { userInfo &&
           <div className="post__controls">
-            <button className="post__control">
-              <i className="far fa-heart"></i>
+            <button 
+              className="post__control"
+              onClick={() => setLike(!like)}
+            >
+              { like
+                ? <i className="fas fa-heart"></i>
+                : <i className="far fa-heart"></i>
+              }
             </button>
 
             <div className="post__status">
               <div className="user">
-                <span>curtido por <Link to="/">{comments[0]?.name}</Link> e outra 
-                {comments.length} pessoas</span>
+                <span>
+                  curtido por <Link to="/">{comments[0]?.name}</Link>&nbsp;
+                  e outra{((comments.length - 1) + like) > 1 && 's'}&nbsp;
+                  <Link to="/">{like ? comments.length : comments.length - 1}</Link>&nbsp; 
+                  pessoa{((comments.length - 1) + like) > 1 && 's'}
+                </span>
               </div>
             </div>
           </div>
--- src/containers/UserForm/UserForm.jsx
+++ src/containers/UserForm/UserForm.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
-// , { useState }
+import React, { useState } from 'react';
 
-// import SuccessMessage from '../../components/SuccessMessage';
+import SuccessMessage from '../../components/SuccessMessage';
 
 import './UserForm.scss';
 
 const UserForm = () => {
+  const [name, setName] = useState('');
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [avatar, setAvatar] = useState('');
+  const [submit, setSubmit] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    fetch('https://5e7d0266a917d70016684219.mockapi.io/api/v1/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name,
+        username,
+        email,
+        avatar
+      })
+    }).then(() => setSubmit(true));
+  }
+
   return (
     <>
       <section className="profile" data-testid="user-form">
@@ -13,31 +35,38 @@ const UserForm = () => {
           <div className="profile-data">
             <div className="user">
               <div className="user__thumb">
-                <img src="https://viniciusvinna.netlify.app/assets/api-instagram/profiles/profile-placeholder.png" alt="perfil"/>
+                { avatar
+                  ? <img src={avatar} alt="perfil"/>
+                  : <img src="https://viniciusvinna.netlify.app/assets/api-instagram/profiles/profile-placeholder.png" alt="perfil"/>
+                }
               </div>
 
               <p className="user__name">
-                name
-                <span>@</span>
+                {name}
+                <span>@{username}</span>
               </p>
             </div>
           </div>
         </div>
       </section>
 
-      <section className="post__form">
+      <form onSubmit={handleSubmit} className="post__form">
         <div className="container">
           <div className="post__form__wrapper">
             <label htmlFor="name">Nome</label>
             <input 
               id="name"
               placeholder="Ex: Fulano da Silva"
+              value={name}
+              onChange={({ target }) => setName(target.value)}
             />
 
             <label htmlFor="user">Usuário</label>
             <input 
               id="user" 
               placeholder="Ex: Fulano_da_Silva"
+              value={username}
+              onChange={({ target }) => setUsername(target.value)}
             />
 
             <label htmlFor="email">Email</label>
@@ -45,18 +74,23 @@ const UserForm = () => {
               type="email" 
               id="email"
               placeholder="Ex: fulano@gmail.com"
+              value={email}
+              onChange={({ target }) => setEmail(target.value)}
             />
 
             <label htmlFor="url-img">Url da Imagem de Perfil (use a url da imagem do Linkedin)</label>
             <input 
               id="url-img"
               placeholder="http://..."
+              onChange={({ target }) => setAvatar(target.value)}
             />
 
-            <button>Cadastrar</button>
+            <button type="submit">Cadastrar</button>
           </div>
         </div>
-      </section>
+      </form>
+  
+      {submit && <SuccessMessage />}
     </>
   );
 };
